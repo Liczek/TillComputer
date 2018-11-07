71f4978a665ac47003bb68831a714eedc29dc95e
Paweł Liczmański <paweliczek@gmail.com>
2018-11-07 22:14:12 +0100

    before color change

--- TillComputer/ComputerProgresController.swift
+++ TillComputer/ComputerProgresController.swift
@@ -14,6 +14,10 @@ class ComputerProgresController: UIViewController {
 	let finishValue: CGFloat = 3000
 	var currentValue: CGFloat = 100
 	var percentage: CGFloat = 0
+	
+	var audioPlayer: AVAudioPlayer?
+	var url: URL?
+	
 	let windowHeight = UIApplication.shared.keyWindow?.frame.height
 	
 	let navigationBarView: NavigationBarView = {
@@ -45,32 +49,109 @@ class ComputerProgresController: UIViewController {
 	let currentMoneyView: CurrentMoneyView = {
 		let view = CurrentMoneyView()
 		view.translatesAutoresizingMaskIntoConstraints = false
-//		view.backgroundColor = .red
 		return view
 	}()
 	
-	var audioPlayer: AVAudioPlayer?
-	var url: URL?
+	var centerXAnchor: NSLayoutConstraint!
+	var centerYAnchor: NSLayoutConstraint!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		let path = Bundle.main.path(forResource: "cha_ching.wav", ofType: nil)!
-		url = URL(fileURLWithPath: path)
+		configureAudioPaths()
+		setupNavigationController()
 		
+		setupUI()
 		
+		navigationBarView.addButton.addTarget(self, action: #selector(addMoney), for: .touchUpInside)
+		updateClearViewHeight(addedValue: 0)
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
 		
+		navigationController?.navigationBar.prefersLargeTitles = false
+		
+	}
+	
+	
+	@objc func addMoney() {
+		let valueToAdd: CGFloat = 10
+		
+		let moneyView: MoneyView = {
+			let view = MoneyView()
+			view.translatesAutoresizingMaskIntoConstraints = false
+			return view
+		}()
+		
+		playChaChingSound()
+		
+		UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+			
+			self.view.addSubview(moneyView)
+			self.setupConstraintsFor(moneyView: moneyView)
+			
+		}) { (true) in
+			UIView.animate(withDuration: 0.3, animations: {
+				self.animateMoneyView(moneyView: moneyView)
+				
+			}, completion: { (true) in
+				moneyView.removeFromSuperview()
+				
+				UIView.animate(withDuration: 0.5, animations: {
+					self.updateClearViewHeight(addedValue: valueToAdd)
+				})
+			})
+		}
+	}
+	
+	fileprivate func updateClearViewHeight(addedValue: CGFloat) {
+		currentValue += addedValue
+		percentage = currentValue / finishValue
+		let windowHeight = componentsView.mainWindowHeight
+		let clearViewHeight = windowHeight! * percentage
+		
+		componentsView.blurViewBottomAnchor.constant = -clearViewHeight
+		componentsView.blurView.layoutIfNeeded()
+		progresBarView.progresViewShadowBottomAnchor.constant = -clearViewHeight
+		progresBarView.progresViewShadow.layoutIfNeeded()
+		progresBarView.progresValueLabel.text = "\((Int(percentage * 100)))%"
+		
+		currentMoneyView.currentMoney = Int(currentValue)
+		currentMoneyView.finishMoney = Int(finishValue)
+		currentMoneyView.configureMoneyLabel()
+	}
+	
+	fileprivate func animateMoneyView(moneyView: MoneyView) {
+		moneyView.transform = CGAffineTransform(scaleX: 1, y: 1)
+		self.centerXAnchor.constant = -150
+		self.centerYAnchor.constant = 100
+		self.view.layoutIfNeeded()
+		moneyView.alpha = 0.2
+	}
+	
+	
+	fileprivate func setupConstraintsFor(moneyView: MoneyView) {
+		NSLayoutConstraint.activate([
+			moneyView.widthAnchor.constraint(equalToConstant: 110),
+			moneyView.heightAnchor.constraint(equalToConstant: 50),
+			])
+		centerXAnchor = moneyView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0)
+		centerXAnchor.isActive = true
+		centerYAnchor = moneyView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0)
+		centerYAnchor.isActive = true
+		moneyView.transform = CGAffineTransform(scaleX: 3, y: 3)
+	}
+	
+	fileprivate func setupNavigationController() {
 		navigationController?.setNavigationBarHidden(true, animated: false)
-		
-		navigationController?.navigationBar.isTranslucent = false
-		navigationController?.navigationBar.barTintColor = .heavyGold
-		navigationController?.navigationBar.tintColor = .shinningGold
-		
-		view.backgroundColor = .heavyGold
-		navigationItem.title = "Till Computer"
-		
 		navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.shinningGold, NSAttributedString.Key.font : UIFont.systemFont(ofSize: 25, weight: UIFont.Weight.bold)]
 		navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.shinningGold, NSAttributedString.Key.font : UIFont.systemFont(ofSize: 25, weight: UIFont.Weight.bold)]
-
+	}
+	
+	fileprivate func setupUI() {
+		view.backgroundColor = .heavyGold
+		
 		view.addSubview(navigationBarView)
 		view.addSubview(progresBarView)
 		view.addSubview(componentsView)
@@ -99,102 +180,23 @@ class ComputerProgresController: UIViewController {
 			currentMoneyView.leadingAnchor.constraint(equalTo: progresBarView.trailingAnchor),
 			currentMoneyView.widthAnchor.constraint(equalToConstant: 40),
 			])
-		
-		
-		navigationBarView.addButton.addTarget(self, action: #selector(addMoney), for: .touchUpInside)
-		updateClearViewHeight(addedValue: 0)
-		
+	}
+
+
+	fileprivate func configureAudioPaths() {
+		let path = Bundle.main.path(forResource: "cha_ching.wav", ofType: nil)!
+		url = URL(fileURLWithPath: path)
+		audioPlayer?.prepareToPlay()
 	}
 	
-	
-	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		navigationController?.navigationBar.prefersLargeTitles = false
-		
-		
-		
-		
-	}
-	
-	
-	@objc func addMoney() {
-		let valueToAdd: CGFloat = 10
-		let moneyView: MoneyView = {
-			let view = MoneyView()
-//			view.backgroundColor = .blue
-			view.translatesAutoresizingMaskIntoConstraints = false
-			return view
-		}()
-		
+	fileprivate func playChaChingSound() {
 		do {
 			audioPlayer = try AVAudioPlayer(contentsOf: url!)
 			audioPlayer?.play()
 		} catch let err {
 			print(err)
 		}
-		
-		var centerXAnchor: NSLayoutConstraint!
-		var centerYAnchor: NSLayoutConstraint!
-
-		
-		UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-			
-			self.view.addSubview(moneyView)
-
-			
-			NSLayoutConstraint.activate([
-				moneyView.widthAnchor.constraint(equalToConstant: 110),
-				moneyView.heightAnchor.constraint(equalToConstant: 50),
-				])
-			centerXAnchor = moneyView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0)
-			centerXAnchor.isActive = true
-			centerYAnchor = moneyView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0)
-			centerYAnchor.isActive = true
-			moneyView.transform = CGAffineTransform(scaleX: 3, y: 3)
-			
-			
-		}) { (true) in
-			UIView.animate(withDuration: 0.3, animations: {
-				moneyView.transform = CGAffineTransform(scaleX: 1, y: 1)
-				centerXAnchor.constant = -150
-				centerYAnchor.constant = 100
-				self.view.layoutIfNeeded()
-				moneyView.alpha = 0.2
-				
-			}, completion: { (true) in
-				moneyView.removeFromSuperview()
-				UIView.animate(withDuration: 0.5, animations: {
-					self.updateClearViewHeight(addedValue: valueToAdd)
-				})
-			})
-			
-			
-		}
-		
-		
-		
-		
-		
 	}
 	
-	func updateClearViewHeight(addedValue: CGFloat) {
-		currentValue += addedValue
-		percentage = currentValue / finishValue
-		let windowHeight = componentsView.mainWindowHeight
-		let clearViewHeight = windowHeight! * percentage
-		
-		componentsView.blurViewBottomAnchor.constant = -clearViewHeight
-		componentsView.blurView.layoutIfNeeded()
-		progresBarView.progresViewShadowBottomAnchor.constant = -clearViewHeight
-		progresBarView.progresViewShadow.layoutIfNeeded()
-		progresBarView.progresValueLabel.text = "\((Int(percentage * 100)))%"
-		
-		currentMoneyView.currentMoney = Int(currentValue)
-		currentMoneyView.finishMoney = Int(finishValue)
-		currentMoneyView.configureMoneyLabel()
-	}
-
-
 }
 
